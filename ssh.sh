865f0de5e19b3331db78b37b96930485a3ff5948
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-12-23 18:01:08 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,1 +1,1 @@
-curl -sSf https://sshx.io/get | sh && sshx
+curl -sSf https://cloud.shellhub.io/install.sh | TENANT_ID=3fc82bcb-2135-4840-bfcc-f8ad9059a26d SERVER_ADDRESS=https://cloud.shellhub.io sh
